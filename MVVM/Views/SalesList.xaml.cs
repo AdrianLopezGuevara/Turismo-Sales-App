fd529ce4860053515247bb304a02539dd6ee713a
HurkiCorgi <al0826503@gmail.com>
2024-07-21 00:29:35 -0600

    feat: Remove commented out code and unused properties in SalesListViewModel

--- MVVM/Views/SalesList.xaml.cs
+++ MVVM/Views/SalesList.xaml.cs
@@ -10,12 +10,6 @@ public partial class SalesList : ContentPage
         var viewModel = BindingContext as SalesListViewModel;
         viewModel!.LoadSalesCommand.Execute(null);
     }
-
-    //private void OnFilterDateChanged(object sender, DateChangedEventArgs e)
-    //{
-    //    var viewModel = (SalesListViewModel)BindingContext;
-    //    viewModel.FilterSalesCommand.Execute(null);
-    //}
     private void OnFilterDateChanged(object sender, DateChangedEventArgs e)
     {
         var viewModel = (SalesListViewModel)BindingContext;
